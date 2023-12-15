464dbe22713f92803d6946e0a5f7183a85822c71
Gr-mm420 <xspace39@stud.fit.vutbr.cz>
2023-12-15 16:55:55 +0100

    Random Name mode working

--- app/src/main/java/com/fititu/logoquizitu/RandomName.kt
+++ app/src/main/java/com/fititu/logoquizitu/RandomName.kt
@@ -1,9 +1,7 @@
 package com.fititu.logoquizitu
 
 import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,105 +16,100 @@ import com.fititu.logoquizitu.Model.AppDatabase
 import com.fititu.logoquizitu.Model.Dao.CompanyDao
 import com.fititu.logoquizitu.Model.Entity.CompanyEntity
 import kotlinx.coroutines.*
-import java.io.File
 
 
 class RandomNameFragment : Fragment() {
-    private var LogoNamesButtons = mutableListOf<Button>()
-    private lateinit var RandomCompanyNames: List<CompanyEntity>
-    private lateinit var CompanyDao: CompanyDao
+    private var logoNamesButtons = mutableListOf<Button>()
+    private lateinit var randomCompanyNames: List<CompanyEntity>
+    private lateinit var companyDao: CompanyDao
     private lateinit var randomLogoImageView: ImageView
+    private lateinit var correctName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        CompanyDao = AppDatabase.getInstance(requireContext()).companyDao()
+        companyDao = AppDatabase.getInstance(requireContext()).companyDao()
         randomLogoImageView = view.findViewById(R.id.randomLogoImage)
         getRandomLogos()
         val logoNamesGridLayout: GridLayout = view.findViewById(R.id.LogoNamesGridLayout)
-        GenerateLogoNameButtons(logoNamesGridLayout)
-
+        generateLogoNameButtons(logoNamesGridLayout)
     }
 
-    private fun getRandomLogos(){
-        RandomCompanyNames = runBlocking(Dispatchers.IO){
-            CompanyDao.getRandomCompanies()
+    private fun getRandomLogos() {
+        randomCompanyNames = runBlocking(Dispatchers.IO) {
+            companyDao.getRandomCompanies()
         }
         lifecycleScope.launch {
             var path_UI: String?
             withContext(Dispatchers.Main) {
-                val path = RandomCompanyNames[0].imgOriginal//randomLogo2.imagePath
+                val path = randomCompanyNames[0].imgOriginal//randomLogo2.imagePath
                 path_UI = path
-
-                Log.d("Image Loading", "other ${Uri.parse(path)}")
             }
             Glide.with(requireContext())
                 .load(path_UI)
                 .into(randomLogoImageView)
         }
     }
-    private fun GenerateLogoNameButtons(gridLayout: GridLayout/*logo entities*/){
+
+    private fun generateLogoNameButtons(gridLayout: GridLayout) {
         gridLayout.rowCount = 4
         gridLayout.columnCount = 1
-
-        for(i in 0..3){
+        correctName = randomCompanyNames[0].companyName
+        randomCompanyNames = randomCompanyNames.shuffled()
+        for (i in 0..3) {
             //todo generate 4 buttons with the 4 logo names
             val button = Button(requireContext())
             val params = GridLayout.LayoutParams()
-
-            button.text = RandomCompanyNames[i].companyName
+            button.text = randomCompanyNames[i].companyName
             button.setBackgroundColor(Color.WHITE)
             button.setOnClickListener {
                 onButtonClicked(button)
             }
             button.setTextColor(Color.BLACK)
             button.setPadding(30, 10, 30, 10)
-            params.rowSpec =GridLayout.spec(i)
+            params.rowSpec = GridLayout.spec(i)
             params.columnSpec = GridLayout.spec(0)
             params.setMargins(10, 10, 10, 10)
             button.layoutParams = params
             button.background = ContextCompat.getDrawable(requireContext(), R.drawable.rounded_button_white)
             gridLayout.addView(button)
-            LogoNamesButtons.add(button)
-
+            logoNamesButtons.add(button)
         }
     }
-    private fun onButtonClicked(button: Button){
-        if(button.text == RandomCompanyNames[0].companyName){
-            for (button in LogoNamesButtons){
-                if(button.text == RandomCompanyNames[0].companyName){
-                    button.background = ContextCompat.getDrawable(requireContext(), R.drawable.rounded_button_green)
+
+    private fun onButtonClicked(button: Button) {
+        if (button.text == correctName) {
+            for (currentButton in logoNamesButtons) {
+                if (currentButton.text == correctName) {
+                    currentButton.background =
+                        ContextCompat.getDrawable(requireContext(), R.drawable.rounded_button_green)
                 }
             }
-        }
-        else{
-            for (button in LogoNamesButtons){
-                if(button.text == RandomCompanyNames[0].companyName){
-                    button.background = ContextCompat.getDrawable(requireContext(), R.drawable.rounded_button_green)
-                }
-                else{
-                    button.background = ContextCompat.getDrawable(requireContext(), R.drawable.rounded_corner)
-
-                }
+        } else {
+            for (currentButton in logoNamesButtons) {
+                if (currentButton.text == correctName)
+                    currentButton.background =
+                        ContextCompat.getDrawable(requireContext(), R.drawable.rounded_button_green)
+                else
+                    currentButton.background = ContextCompat.getDrawable(requireContext(), R.drawable.rounded_corner)
             }
         }
         changeFragmentAfterDelay()
-
     }
-    private fun changeFragmentAfterDelay(){
+
+    private fun changeFragmentAfterDelay() {
         CoroutineScope(Dispatchers.Main).launch {
             delay(1000)
             val fragmentManager = requireActivity().supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
-
-            // Replace the current fragment with the new one
             val newFragment = RandomNameFragment()
             fragmentTransaction.replace(R.id.mainMenuFragmentContainer, newFragment)
-            fragmentTransaction.addToBackStack(null) // Optional: Add to back stack for back navigation
-            fragmentTransaction.commit()//navigate to next fragment
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
         }
     }
 
@@ -124,7 +117,6 @@ class RandomNameFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_random_name, container, false)
     }
 
